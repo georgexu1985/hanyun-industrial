80a456ee47c03cf3503eec72b6d61d9948c70c11
george <xujj86@126.com>
2021-01-05 11:03:45 +0800

    测试日志

--- src/main/java/com/hanyun/industrial/IndustrialApplication.java
+++ src/main/java/com/hanyun/industrial/IndustrialApplication.java
@@ -2,8 +2,10 @@ package com.hanyun.industrial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IndustrialApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/hanyun/industrial/scheduling/SchedulingService.java
@@ -0,0 +1,17 @@
+package com.hanyun.industrial.scheduling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SchedulingService {
+
+    private static int count = 0;
+
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void init() {
+        log.info("##############SchedulingService ================== " + count++);
+    }
+}
